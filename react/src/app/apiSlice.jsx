13e15fd88f97842836db82217106e2247eb09cd6
dvori5473 <165666022+dvori5473@users.noreply.github.com>
2024-11-13 14:14:49 +0200

    change base url

--- react/src/app/apiSlice.jsx
+++ react/src/app/apiSlice.jsx
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 const apiSlice = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:1234",
+        baseUrl: "https://server-knc7.onrender.com/",
         credentials: 'include',
         prepareHeaders: (headers, { getState }) => {
             const token = getState().auth.token
